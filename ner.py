f529ff0764a4199573ef71016a4f9f9f2c26f224
mzn32qwq <71418204+mzn32qwq@users.noreply.github.com>
2023-12-01 15:30:42 +0100

    Update ner.py

--- ner.py
+++ ner.py
@@ -5,6 +5,10 @@ import re
 nlp = spacy.load("en_core_web_sm")
 import json
 import csv
+from ctransformers import AutoModelForCausalLM
+repository = 'TheBloke/Llama-2-13B-chat-GGUF'
+model_file = 'llama-2-13b-chat.Q4_K_M.gguf'
+llm = AutoModelForCausalLM.from_pretrained(repository, model_file=model_file, model_type='llama')
 
 def tokenize(text):
     # break text into tokens
@@ -16,17 +20,20 @@ def extract_entities_from_json(json_file,train_file):
     #open output file
     with open(train_file, 'w', newline='', encoding='utf-8') as out_file:
         csv_writer = csv.writer(out_file)
-        csv_writer.writerow(['Question', 'Answer'])  # write header row
+        csv_writer.writerow(['Question', 'Answer',"question_entity","answer_raw","answer_entity"])  # write header row
 
         for item in data:
             question = item['question']
             answers = item['answer']
+            raw_answer = llm(question)
             # tokenize question
             question_tokens = tokenize(question)
             question_tokens = nlp(question)
             entities = ', '.join([f"{ent.text} ({ent.label_})" for ent in question_tokens.ents])
-            item['entities'] = entities
-            print(f"Question: {question}\nEntities: {entities}\n")
+            raw_answer=tokenize(raw_answer)
+            raw_answer=nlp(raw_answer)
+            extracted_answer = ', '.join([f"{ent.text} ({ent.label_})" for ent in raw_answer.ents])
+            csv_writer.writerow([question, answers, entities, raw_answer, extracted_answer])
 
 
 
