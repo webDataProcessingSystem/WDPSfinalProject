bf5e3d0e50b17e12956f41755fc5fcdcc67711c3
Wtian1001 <tian.wang2@student.uva.nl>
2023-12-01 12:20:41 +0100

    starter.py

---
+++ starter.py
@@ -0,0 +1,26 @@
+from ctransformers import AutoModelForCausalLM
+repository = 'TheBloke/Llama-2-13B-chat-GGUF'
+model_file = 'llama-2-13b-chat.Q4_K_M.gguf'
+llm = AutoModelForCausalLM.from_pretrained(repository, model_file=model_file, model_type='llama')
+
+def starter(file_name):
+    # clear answer file
+    with open('answer.txt', 'w') as f:
+        f.write('')
+    # read question file
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            question_id = line.split('    ')[0]
+            question = line.split('    ')[1]
+            print(question_id)
+            print(question)
+            # generate answer
+            answer = llm(question)
+            print('Raw Answer: ' + answer)
+            # write answer
+            with open('answer.txt', 'a') as answer_file:
+                answer_file.write(question_id + '    R' + '"' + answer + '"' + '\n')
+
+
+starter('question.txt')
