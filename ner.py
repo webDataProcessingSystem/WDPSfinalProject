27e9e0dc61c44990f654bdc2c30ecce84fefb46f
mzn32qwq <71418204+mzn32qwq@users.noreply.github.com>
2023-12-05 21:36:00 +0100

    Update ner.py

--- ner.py
+++ ner.py
@@ -5,8 +5,8 @@ import re
 nlp = spacy.load("en_core_web_sm")
 import json
 import csv
-from transformers import LlamaForCausalLM, LlamaTokenizer
- 
+# from transformers import LlamaForCausalLM, LlamaTokenizer
+from ctransformers import AutoModelForCausalLM 
 
 
 def tokenize(text):
@@ -15,8 +15,9 @@ def tokenize(text):
 
 def extract_entities_from_json(json_file,train_file,model_path,tokenizer_path):
     # tokenizer = LlamaTokenizer.from_pretrained(tokenizer_path)
-    model = LlamaForCausalLM.from_pretrained(model_path)
-    model.eval()
+    # model = LlamaForCausalLM.from_pretrained(model_path)
+    model = AutoModelForCausalLM.from_pretrained(model_path_or_repo_id=model_path, local_files_only=True, model_type='llama')
+    # model.eval()
     with open(json_file, 'r', encoding='utf-8') as file:
         data = json.load(file)
     #open output file
@@ -28,14 +29,19 @@ def extract_entities_from_json(json_file,train_file,model_path,tokenizer_path):
             question = item['question']
             answers = item['answer']
             raw_answer = model(question)
+            print("Question:" + question)
+            print('Raw Answer:' + raw_answer)
+            print('Answers:' )
+            print(answers)
+            print('End')
             # tokenize question
-            question_tokens = tokenize(question)
-            question_tokens = nlp(question)
-            entities = ', '.join([f"{ent.text} ({ent.label_})" for ent in question_tokens.ents])
-            answers_tokens=tokenize(raw_answer)
-            answers_tokens=nlp(answers_tokens)
-            extracted_answer = ', '.join([f"{ent.text} ({ent.label_})" for ent in answers_tokens.ents])
-            csv_writer.writerow([question, answers, entities, raw_answer, extracted_answer])
+            # question_tokens = tokenize(question)
+            # question_tokens = nlp(question)
+            # entities = ', '.join([f"{ent.text} ({ent.label_})" for ent in question_tokens.ents])
+            # answers_tokens=tokenize(raw_answer)
+            # answers_tokens=nlp(answers_tokens)
+            # extracted_answer = ', '.join([f"{ent.text} ({ent.label_})" for ent in answers_tokens.ents])
+            # csv_writer.writerow([question, answers, entities, raw_answer, extracted_answer])
 
 
 
