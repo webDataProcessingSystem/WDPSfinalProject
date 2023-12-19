cde3544c90b16b961e44f12a2b6ddb9a5ce5f3ca
Cedar_Cheng <70186814+DaydreamCreator@users.noreply.github.com>
2023-12-19 20:50:06 +0100

    add write_output()

--- project/src/IOFunc.py
+++ project/src/IOFunc.py
@@ -22,9 +22,30 @@ def get_input(input_str: str, is_verbosed: bool) -> list:
         lines = file.readlines()
         for line in lines:
             q_id, question = tuple(re.split(r' {4}|\t', line.strip()))
+            pattern = r"Question: "
+            matchObj = re.match(pattern, question)
+            question = question if matchObj == None else question[10:]
             q_list.append({"q_id": q_id, "question": question})
     verbose("### 2> Opened file " + input_str +" and generated question_list ...", is_verbosed)
     return q_list
+
+def write_ouput(outpath: str, q_id: str, raw_answer: str, answer: str, answer_checked: str, entites: dict):
+    """
+    raw_answer : R
+    entities: E
+    answer: A
+    answer_checked: C
+    """
+    with open(outpath, 'a') as file:
+        raw_answer = raw_answer.replace('"', "'")
+        prefix = q_id + "\t"
+        file.write( prefix + "R\"" + raw_answer + "\"\n")
+        file.write( prefix + "A\"" + answer + "\"\n")
+        file.write( prefix + "C\"" + answer_checked + "\"\n")
+        for ent in entites:
+            file.write( prefix + "E\"" + ent['entity'] + "\"\t\"" + ent['wiki_link'] + "\"\n")
+
+    
 
 def get_answer_from_llm(question: str, is_verbosed: bool, llm_name: str=model_file, repository_name: str=repository):
     llm = AutoModelForCausalLM.from_pretrained(repository_name, model_file=llm_name, model_type="llama")
