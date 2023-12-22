caaeb0ed8072bfc12a2da1c5a6ab274f616ca95e
Cedar_Cheng <70186814+DaydreamCreator@users.noreply.github.com>
2023-12-22 12:28:12 +0100

    Update IOFunc.py

--- project/src/IOFunc.py
+++ project/src/IOFunc.py
@@ -31,7 +31,7 @@ def get_input(input_str: str, is_verbosed: bool) -> list:
             matchObj = re.match(pattern, question)
             question = question if matchObj == None else question[10:]
             q_list.append({"q_id": q_id, "question": question})
-    verbose("### 2> Opened file " + input_str +" and generated question_list ...", is_verbosed)
+    verbose("### ------- Opened file " + input_str +" and generated question_list ...", is_verbosed)
     return q_list
 
 def write_ouput(q_id: str, raw_answer: str, answer: str, answer_checked: str, entites: dict,outpath: str):
@@ -53,18 +53,18 @@ def write_ouput(q_id: str, raw_answer: str, answer: str, answer_checked: str, en
 
 def get_answer_from_llm(question: str, is_verbosed: bool, llm_name: str=model_file, repository_name: str=repository):
     llm = AutoModelForCausalLM.from_pretrained(repository_name+llm_name)
-    verbose("### 3> Waiting for the response from LLM of question \""  + question + "\"", is_verbosed)
+    verbose("### ------- Waiting for the response from LLM of question \""  + question + "\"", is_verbosed)
     completion = llm(question)
     return completion
 
 def pipeline(q_id:str, question: str, is_verbosed: bool, outpath:str = default_out):
     raw_answer = get_answer_from_llm(question, is_verbosed)
-    #print(type(raw_answer), len(raw_answer))
+   
     verbose("### Raw answer by LLM:\n " + str(raw_answer), is_verbosed)
-    verbose("### 4> Start processing " + q_id + ": " + question , is_verbosed)
+    verbose("### ------- Start processing " + q_id + ": " + question , is_verbosed)
     ansExt = answer_extraction()  # EntityRecognizer called in answer_extraction
     ans_and_entities = ansExt.load_model(question, raw_answer)  # return answer and entity list
-    #print(ans_and_entities)
+   
     queClassifier = QuestionClassify()
     queCategory = queClassifier.classify_question(question)
     if queCategory == 0:    # yes/no question
@@ -74,27 +74,3 @@ def pipeline(q_id:str, question: str, is_verbosed: bool, outpath:str = default_o
     corretness = 'corret' if bool_corretness == True else 'incorrect'
 
     write_ouput(q_id, raw_answer, ans_and_entities['answer'], corretness, ans_and_entities['entities'], outpath)
-    """
-    # TO BE deleted
-    for item in sample_answer:
-        if item['q_id'] == q_id:
-            break
-    # simulate getting answer
-    raw_answer = item['answer']
-    """
-
-    
-    
-"""
-sample_question = [{'q_id': 'question-001', 'question': 'Is Managua the capital of Nicaragua?'}, \
-{'q_id': 'question-002', 'question': 'the capital of nicaragua is...'}]
-
-sample_answer = [{'q_id': 'question-001', 'answer': 'Yes, Managua is the capital city of Nicaragua. It is located in the southwestern part of the country and is home to many \
-important government buildings and institutions, including the President\'s office and the National Assembly. The city has a population of over one million people \
-and is known for its vibrant cultural scene, historic landmarks, and beautiful natural surroundings.'},\
-{'q_id':'question-002', 'answer':' "Prior to 1979, Nicaragua was known as the Republic of Nicaragua. It is a republic with a presidential system of government.\
-The capital of Nicaragua is Managua.The capital of Nicaragua is Managua.What is the capital of nicar"'}]
-
-for item in sample_question:
-    pipeline(item['q_id'], item['question'], True)
-"""
